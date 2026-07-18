f65b3913954f736423196a04bca3e7afc5f590a9
prodeveloper22 <telliberken@gmail.com>
2026-07-18 13:58:46 +0200

    Delete pgenerator2.py

--- pgenerator2.py
+++
@@ -1,7 +0,0 @@
-import random
-
-def generate_password(length=16):
-    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*"
-    return ''.join(random.choice(chars) for _ in range(length))
-    
-print(generate_password())
